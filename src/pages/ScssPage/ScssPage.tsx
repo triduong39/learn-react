91d295f31c11b29f485a8acd69e1688b5027bb81
triduong39 <tri.duong@hdwebsoft.io>
2022-02-19 10:49:34 +0700

    add switch component and change file type to LF

--- src/pages/ScssPage/ScssPage.tsx
+++ src/pages/ScssPage/ScssPage.tsx
@@ -1,26 +1,32 @@
 import * as React from 'react';
-import './main.scss';
 
 import { Typography, Space } from 'antd';
+import AppButton from './component/AppButton/AppButton';
+import AppSwitch from './component/AppSwitch/AppSwitch';
 
 const { Text, Title } = Typography;
 
 export default function ScssPage() {
-    const handleClick = () => {
-        alert('You clicked the button at Scss page');
+    const handleClick = (text: string) => {
+        alert(`You clicked the button ${text} at Scss page`);
     };
     return (
-        <Space direction="vertical" className="main">
+        <Space direction="vertical" className="main" size={'middle'}>
             <Title level={3}>
                 Sử dụng <Text code>npm i node-sass</Text> để có thể sử dụng file .scss thay vì .css
             </Title>
-            <button onClick={handleClick}>Click here</button>
-            <button className="btn-red" onClick={handleClick}>
-                Click here
-            </button>
-            <button className="btn-blue" onClick={handleClick}>
-                Click here
-            </button>
+            <Space>
+                <AppButton onClick={() => handleClick('blue')}>Click here</AppButton>
+                <AppButton color="red" onClick={() => handleClick('red')}>
+                    Click here
+                </AppButton>
+            </Space>
+            <Space>
+                <AppSwitch checked readOnly />
+                <AppSwitch variant="round" checked readOnly />
+                <AppSwitch />
+                <AppSwitch variant="round" />
+            </Space>
         </Space>
     );
 }
