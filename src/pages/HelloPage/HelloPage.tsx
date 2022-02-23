0ab59452018d3e52da1a9d0a2833dcb1cd531ba6
triduong39 <tri.duong@hdwebsoft.io>
2022-02-23 12:09:06 +0700

    beautify files

--- src/pages/HelloPage/HelloPage.tsx
+++ src/pages/HelloPage/HelloPage.tsx
@@ -1,23 +1,19 @@
-import * as React from "react";
-import { Input, Space, Typography } from "antd";
+import * as React from 'react';
+import { Input, Space, Typography } from 'antd';
 
-import HelloWorld from "./Component/HelloWorld";
+import HelloWorld from './Component/HelloWorld';
 const { Title } = Typography;
 
 export default function HelloPage() {
-  const [text, setText] = React.useState("");
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setText(event.target.value);
-  };
-  return (
-    <Space direction="vertical" size={"large"}>
-      <Title level={3}>“Hello World” component</Title>
-      <Input
-        placeholder="Type some text"
-        value={text}
-        onChange={handleChange}
-      />
-      <HelloWorld text={text}></HelloWorld>
-    </Space>
-  );
+    const [text, setText] = React.useState('');
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setText(event.target.value);
+    };
+    return (
+        <Space direction="vertical" size={'large'}>
+            <Title level={3}>“Hello World” component</Title>
+            <Input placeholder="Type some text" value={text} onChange={handleChange} />
+            <HelloWorld text={text}></HelloWorld>
+        </Space>
+    );
 }
